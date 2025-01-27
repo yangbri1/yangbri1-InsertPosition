5963cbe65ced00f21b1cc5d5ce5f87d84cb9bc21
Brian Yang <yangbri1@msu.edu>
2025-01-27 02:30:12 +0000

    Use control flow (while loop, if statements) to find appropriate index where given int 'target' would settle within int[] array 'nums'.

--- src/main/java/InsertPosition.java
+++ src/main/java/InsertPosition.java
@@ -1,4 +1,4 @@
-
+// import java.util.Arrays;     // --- import 'Arrays' class from Java's utility package to implement Arrays.sort() if not already ...
 
 public class InsertPosition {
 
@@ -15,6 +15,27 @@ public class InsertPosition {
      * @return return the index that a number should be inserted into a sorted array.
      */
     public int searchInsert(int[] nums, int target) {
-        return -1;
+        // if 'nums' is an empty int array ...
+        if(nums.length == 0){
+            // inserted 'target' would be the very 1st elem in the array --- hence at index 0
+            return 0;
+        }
+        /* otw if 'nums' is NOT an empty int array ... */
+        // initialize 'i' to be iterator for while-loop
+        int i = 0;
+        // iterate via int[] array 'nums' ... --- w/ the given presumption that it is already sorted ...
+        // java.util.Arrays.sort(nums);     // --- enable this if 'nums' ary NOT already sorted
+        while(i < nums.length){
+            // if current num is greater than given 'target' ...
+            if(nums[i] > target){
+                // return index number -- this is where 'target' would be inserted at
+                return i;
+            }
+            // increment by 1 to keep while-loop operating
+            ++i;
+        }
+        // under some circumstances the ary is 1) NOT empty, 2) array is filled with int less than 'target' ...
+        /* 'target' would be inserted at end of int[] array 'nums' */
+        return (nums.length);
     }
 }
